dc3b2ba41d935108eaf0058c895fa23018ba2e2e
ichibuike <emmanuelchibuikenduka2@gmail.com>
2025-05-20 01:40:48 +0100

    Add files via upload

---
+++ Tayabou/products.js
@@ -0,0 +1,16 @@
+let products = '';
+const productNmae= '';
+let userNmae = '';
+
+function store(products, productNmae, userNmae) {
+    name = null;
+    if (name === null) {
+        TouchEvent.apply('statements');
+    }else if (name === '') {
+        TouchEvent.apply('Please place your order');
+    }else if (name.lenght === 4) {
+        Touch('Alert')
+    };
+
+    return name();
+}
